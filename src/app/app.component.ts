d9dc57f3793a7ac5cc46fece2f01e5b6c16dd4c8
hasandi_palliaguruge <pghasandi@gmail.com>
2019-10-03 20:49:32 +0530

    onSubmit event added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,8 @@ export class AppComponent {
 
  userModel = new User('Lila','lila@gmail.com',+9476271431,'default','morning',true);
 
+constructor(private _enrollmentService: EnrollmentService) {}
+
  validateTopic(value){
   if(value==='default'){
     this.topicHasError=true;
@@ -20,4 +23,14 @@ export class AppComponent {
     this.topicHasError=false;
   }
  }
+
+onSubmit(){
+ this._enrollmentService.enroll(this.userModel)
+ .subscribe(
+   data=>console.log('Success!',data),
+   error=>console.error('Error!',error)
+   
+ )
+}
+
 }
